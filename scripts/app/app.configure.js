9f28a1a23e7a337fd193653b265c3432e3d67062
Toma Puljak <toma.puljak@hotmail.com>
2017-06-08 21:02:58 +0200

    a

--- scripts/app/app.configure.js
+++ scripts/app/app.configure.js
@@ -1,13 +1,3 @@
 ﻿angular.module('app', [
     "LocalStorageModule"
 ]);
-
-anuglar.module('app').config(function ($locationProvider, $stateProvider) {
-    $locationProvider.html5Mode(true);
-    $stateProvider
-        .state('newsList', {
-            url: '/',
-            controller: "NewsListController",
-            templateUrl: 
-        })
-})
